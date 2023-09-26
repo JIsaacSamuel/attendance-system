7d63a7e5da21f4d422bb7942ef8b0398d73ef916
JIsaacSamuel <isij2018@gmail.com>
2023-09-26 15:19:59 +0530

    Day 2 commit 3

--- schoolmana/base/models.py
+++ schoolmana/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    school_id = models.IntegerField()
+    school_admin = models.CharField()
+
+class Room(models.Model):
+    class_id = models.IntegerField()
+    class_name = models.IntegerChoices()
+    class_div = models.CharField(max_length=2)
+    class_rep = models.CharField(max_length=40, null=True, blank=True)
+
+# class Students(models.Model):
